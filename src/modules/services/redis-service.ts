7e2362c1339f9c508a3e1aa320e6e266404b935b
syibromalisi <m.syibromalisi@gmail.com>
2021-07-14 14:43:08 +0700

    add sample for class & redis add value to list

--- src/modules/services/redis-service.ts
+++ src/modules/services/redis-service.ts
@@ -10,3 +10,26 @@ export const setLoginRedis = (param, server) => new Promise((resolve, reject) =>
         }
     })
 });
+
+export class RedisOperation {
+    // server: any;
+    redis: any;
+    conf: any;
+
+    constructor(serverInstance) {
+        this.redis = serverInstance.redis;
+        this.conf = serverInstance.conf;
+    }
+
+    setValueToList = (key, value) => new Promise((resolve, reject) => {
+
+        // key login token
+        this.redis.sadd(key, value, (err, val) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(val);
+            }
+        })
+    });
+}
